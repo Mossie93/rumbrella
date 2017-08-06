e0e69485dc9f7d77fdd1f145ab11a5e9de153949
Rafał Mossakowski <rafal.mossakowski@netguru.pl>
2017-08-06 21:37:06 +0200

    Add test for Rumbl.UserSocket authentication with invalid token

--- apps/rumbl/test/channels/user_socket_test.exs
+++ apps/rumbl/test/channels/user_socket_test.exs
@@ -7,4 +7,9 @@ defmodule Rumbl.Channels.UserSocketTest do
     assert {:ok, socket} = connect(UserSocket, %{"token" => token})
     assert socket.assigns.user_id == "123"
   end
+
+  test "socket authentication with invalid token" do
+    assert :error = connect(UserSocket, %{"token" => "1234"})
+    assert :error = connect(UserSocket, %{})
+  end
 end
